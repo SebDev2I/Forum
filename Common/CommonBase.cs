c217197bd625ae45274fc76314c765eb8522e022
SebDev2i <sebastien.nosjean@2isa.org>
2016-11-26 15:38:27 +0100

    Création Common(DTO)

---
+++ Common/CommonBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Common
+{
+    public class CommonBase
+    {
+        //Valeurs null de configuration
+        public static int Int_NullValue = int.MinValue;
+        public static float Float_NullValue = float.MinValue;
+        public static decimal Decimal_NullValue = decimal.MinValue;
+        public static string String_NullValue = null;
+        public static DateTime DateTime_NullValue = DateTime.MinValue;
+    }
+}
---
+++ Common/RegisteredDTO.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class RegisteredDTO : DTOBase
+    {
+        public int IdUser { get; set; }
+        public int StatusUser { get; set; }
+        public int TrainingUser { get; set; }
+        public string NameUser { get; set; }
+        public string FirstnameUser { get; set; }
+        public string EmailUser { get; set; }
+        public string LoginUser { get; set; }
+        public string PwdUser { get; set; }
+        public string KeywordUser { get; set; }
+
+        public RegisteredDTO()
+        {
+            IdUser = Int_NullValue;
+            StatusUser = Int_NullValue;
+            TrainingUser = Int_NullValue;
+            NameUser = String_NullValue;
+            FirstnameUser = String_NullValue;
+            EmailUser = String_NullValue;
+            LoginUser = String_NullValue;
+            PwdUser = String_NullValue;
+            KeywordUser = String_NullValue;
+            IsNew = true;
+        }
+    }
+
+}
