23cecc616a7ad56f7077f4de96b4e2b8eb964083
SebDev2i <sebastien.nosjean@2isa.org>
2017-01-11 09:12:12 +0100

    Revert "modifier couleur"
    This reverts commit d83a6db879608cd2ea105bbc722aa7e6d4b2fe60.

--- FIISA_Universel/FIISA_Universel.Windows/MVVM/Views/MainPage.xaml.cs
+++ FIISA_Universel/FIISA_Universel.Windows/MVVM/Views/MainPage.xaml.cs
@@ -214,9 +214,7 @@ namespace FIISA_Universel
             }
             else cmdAddMessage.Visibility = Visibility.Visible;
 
-            ListViewItem l = ((sender as ListView).SelectedItem as ListViewItem);
-
-            ShowEditDeleteMessage("EditDelete", lstMessage);
+            ShowEditDeleteMessage("EditDelete");
         }
         
         private void cmdAddTopic_Click(object sender, RoutedEventArgs e)
@@ -452,10 +450,7 @@ namespace FIISA_Universel
 
         private void cmdCancelMessageEdit_Click(object sender, RoutedEventArgs e)
         {
-            EditMessage.Visibility = Visibility.Collapsed;
-            cmdAddMessage.Visibility = Visibility.Visible;
-            txtContentMessage.Document.SetText(Windows.UI.Text.TextSetOptions.FormatRtf, string.Empty);
-            cmdAddTopic.Visibility = Visibility.Visible;
+
         }
 
         private void ShowEditDeleteTopic(string ctrlname)
@@ -471,11 +466,11 @@ namespace FIISA_Universel
                 }
             }
         }
-        private void ShowEditDeleteMessage(string ctrlname, DependencyObject dobject)
+        private void ShowEditDeleteMessage(string ctrlname)
         {
             if (mainVM.IsLogged && mainVM.MyRegistered.ObjStatus.NameStatus != "Stagiaire")
             {
-                foreach (var stackpanel in FindVisualChildren<StackPanel>(dobject))
+                foreach (var stackpanel in FindVisualChildren<StackPanel>(lstMessage))
                 {
                     if (stackpanel.Name == ctrlname)
                     {
