a28f5d23850f008147d9a4e2e53109c7372df622
SebDev2i <sebastien.nosjean@2isa.org>
2017-01-16 19:04:30 +0100

    Modification doc

--- DLLForumV2/ForumBase.cs
+++ DLLForumV2/ForumBase.cs
@@ -5,6 +5,9 @@ using System.Text;
 
 namespace DLLForumV2
 {
+    /// <summary>
+    /// Classe de base permettant d'affecter des valeurs null et générer une liste d'erreur lors de la validation de mes propriétés de classe
+    /// </summary>
     public abstract class ForumBase
     {
         public static int Int_NullValue = int.MinValue;
@@ -14,10 +17,17 @@ namespace DLLForumV2
         public static DateTime DateTime_NullValue = DateTime.MinValue;
         public List<ValidationError> ValidationErrors { get; set; }
 
+        /// <summary>
+        /// Constructeur par défaut
+        /// </summary>
         public ForumBase()
         {
             ValidationErrors = new List<ValidationError>();
         }
+        /// <summary>
+        /// Méthode abstraite retournant une liste de ValidationError
+        /// </summary>
+        /// <returns></returns>
         public abstract List<ValidationError> Validate();
     }
 }
--- DLLForumV2/ValidationError.cs
+++ DLLForumV2/ValidationError.cs
@@ -6,12 +6,36 @@ using System.Threading.Tasks;
 
 namespace DLLForumV2
 {
+    /// <summary>
+    /// Classe permettant d'assigner des erreurs de validation
+    /// </summary>
     public class ValidationError
     {
+        /// <summary>
+        /// Nom de la propriété concernée par l'erreur
+        /// </summary>
         public string PropertyName { get; set; }
+
+        /// <summary>
+        /// Information au sujet de l'erreur
+        /// </summary>
         public string Information { get; set; }
+
+        /// <summary>
+        /// Liste de ValidationError
+        /// </summary>
         public List<ValidationError> ValidationErrors { get; set; }
+
+        /// <summary>
+        /// Constructeur par défaut
+        /// </summary>
         public ValidationError() { }
+
+        /// <summary>
+        /// Constructeur complet
+        /// </summary>
+        /// <param name="propertyname"></param>
+        /// <param name="information"></param>
         public ValidationError(string propertyname, string information) : this()
         {
             PropertyName = propertyname;
